9d1ea343536c43a65832cf7edd995404f2defef3
Refkinscallv <refkinscallv@gmail.com>
2025-06-02 14:14:40 +0700

    [FIX] versioning & node.js package actions

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@
  * @description Laravel-style routing system for Express.js.
  * @author Refkinscallv
  * @repository https://github.com/refkinscallv/express-routing
- * @version 1.1.0
+ * @version 1.1.1
  * @date 2025
  */
 module.exports = class Routes {
